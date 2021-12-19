64aa7841b7687714f0719150383e3b9a9c3a37c5
TamarEldad <tamar.eldad@tipalti.com>
2021-12-19 21:25:56 +0200

    new file employee

---
+++ Models/Bamboo/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tipalti.TheWho.Models.Bamboo
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string WorkPhone { get; set; }
+        public string BestEmail { get; set; }
+        public string Supervisor { get; set; }
+        public int? SupervisorEId { get; set; }
+    }
+}
